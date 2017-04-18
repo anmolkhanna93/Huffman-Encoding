abab7c5376e4f7c72fb654cf6feee0f5aa70dffc
anmolkhanna93 <anmolkhanna93@gmail.com>
2017-04-17 21:53:28 -0400

    Node file for decoder tree

---
+++ Node.java
@@ -0,0 +1,43 @@
+
+
+public class Node 
+{
+    public int element;
+    
+    public Node left;
+    public Node right;
+    
+    public Node(int element)
+    {
+    	super();
+    	this.element = element;
+    }
+
+	public int getElement() {
+		return element;
+	}
+
+	public void setElement(int element) {
+		this.element = element;
+	}
+
+	public Node getLeft() {
+		return left;
+	}
+
+	public void setLeft(Node left) {
+		this.left = left;
+	}
+
+	public Node getRight() {
+		return right;
+	}
+
+	public void setRight(Node right) {
+		this.right = right;
+	}
+    
+
+  
+    
+}
